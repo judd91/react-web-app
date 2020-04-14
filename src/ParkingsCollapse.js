c4fc7a818d736c3ece240dbcf7502c1167490fcf
Jud <12875132+judd91@users.noreply.github.com>
2020-04-15 01:36:02 +0200

    improving accordion - not complete

--- src/ParkingsCollapse.js
+++ src/ParkingsCollapse.js
@@ -6,27 +6,68 @@ import { Button, CardBody, CardText, Row, Col, Collapse } from 'reactstrap';
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 
-export default function ParkingsCollapse() {
+
+export default class ParkingsCollapse extends Component {
+  constructor(props) {
+    super(props)
+    this.elements = data.children;
+  }
+  render() {
     return (
       <div className="parkingscollapse">
-        <GetTree />        
+        <Row >
+          <Accordion>
+          { this.elements.map(({name,children},i) => {
+            const todoItems = children.map((item,j) => {
+              // const parkings = item.children.map((parking,k) => <p key={k}> {parking.NOMBRE} </p> )
+            return (<p key={j}> {item.name} </p>)
+            } 
+            )
+            return (
+            <Card key={i}>
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey={"key_"+i.toString()}>
+                  {name}
+                </Accordion.Toggle>
+              <Accordion.Collapse eventKey={"key_"+i.toString()}>
+                {/* {children.map((aaa,j) => <p key={j}> {aaa.name} </p>
+                  )} */}
+                <Card.Body key={i}>{todoItems}</Card.Body>
+                {/* {this.kids.map((item, j) =>
+                  
+                )}  */}
+              </Accordion.Collapse>
+            </Card>
+    )})}
+          </Accordion>
+        </Row>       
       </div>
     )
+  }
 }
 
+//ReactDOM.render(<ParkingsCollapse/>, document.getElementById('app'));
+
+// export default function ParkingsCollapse() {
+    
+// }
+
 function GetTree(){
-  const elements = data.children;
+  
   const [open, setOpen] = useState(false);
+  
   return (
     <Row >
       <Accordion>
-      {elements.map((element,i) => ( 
-      <Card>
+      {this.elements.map(({name,children},i) => (
+      <Card key={i}>
           <Accordion.Toggle as={Card.Header} variant="link" eventKey={"key_"+i.toString()}>
-            {element.name}
+            {name}
           </Accordion.Toggle>
         <Accordion.Collapse eventKey={"key_"+i.toString()}>
-          <Card.Body>Hello! I'm the body</Card.Body>
+          <Card.Body>sdsdsd</Card.Body>
+          {/* {this.kids.map((item, j) =>
+            
+           )}  */}
         </Accordion.Collapse>
       </Card>
       ))}
