5be64bd118d87c5b84b89f77e2e720bdd4dc0980
Jud <12875132+judd91@users.noreply.github.com>
2020-04-21 03:14:40 +0200

    toggle loop ok - dropdown table

--- src/ShowAll.js
+++ src/ShowAll.js
@@ -1,7 +1,7 @@
 import React, { Component, useState  } from 'react';
 import ReactDOM from 'react-dom';
 import data from './data/file.js';
-import { Card, Button, CardTitle, CardText, Row, Col, Table , ButtonGroup , CardBody, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import { Dropdown , DropdownToggle, DropdownMenu, DropdownItem, Card, Button, CardTitle, CardText, Row, Col, Table , ButtonGroup , CardBody, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,29 +21,9 @@ export default class ShowAll extends Component{
     this.aparcamientos = data.item;
 
     this.state = {
-      modalinfo: "",
-      modaltitle: ""
+      isOpen: false,
     }
-    this.handleShow = this.handleShow.bind(this);
 
-    this.show = false
-  }
-  handleShow(e, id, info) {
-    if(info == null){
-      this.setState({ 
-        modalinfo: "There is no information",
-        modaltitle: id.NOMBRE
-      })
-    } else {
-      console.log(id)
-      this.setState({ 
-        modalinfo: info,
-        modaltitle: id.NOMBRE
-      })
-    }
-     
-    this.show = !this.show;
-    console.log(this.show)
   }
 
   render(){
@@ -56,8 +36,6 @@ export default class ShowAll extends Component{
             <th>Name</th>
             <th>Area</th>
             <th>District</th>
-            {/* <th>Adress</th> */}
-            {/* <th>P.C.</th> */}
             <th>Type</th>
             <th>Description</th>
             <th>Info</th>
@@ -66,36 +44,22 @@ export default class ShowAll extends Component{
         <tbody>
         {this.aparcamientos.map(element => {
           const name_p = element.NOMBRE.split(".")
+
           return (
-            // <Col xs="5" sm="4" md="3" key={element.PK}>
               <tr key={element.PK}>
                 <td>{name_p[1]}</td>
-                {/* <td>{element.NOMBRE[0]}</td> */}
                 <td>{element.BARRIO}</td>
                 <td>{element.DISTRITO}</td>
                 <td>{name_p[0]}</td>
-                <td>{element.DESCRIPCION}</td>
-                {/* <td>{element.DIRECCION}</td> */}
-                {/* <td>{element["CODIGO-POSTAL"]}</td> */}
+                {/* <td>{element.DESCRIPCION}</td> */}
                 <td>
-                  {/* <Button color="primary" id={"toggler" + element.PK} style={{ marginBottom: '1rem' }}> + </Button> */}
-                  <ButtonGroup size="sm">
-                    <Button onClick={(e)=>this.handleShow(e, element, element.TELEFONO)} ><FaPhone/></Button>
-                    <Button onClick={(e)=>this.handleShow(e, element, element.EMAIL)}><FaRegEnvelope/></Button>
-                    <Button onClick={(e)=>this.handleShow(e, element, element.DIRECCION + ", " + element["CODIGO-POSTAL"] + ", Madrid")}><FaMapMarkerAlt/></Button>
-                    <Button href={element["CONTENT-URL"]}><FaGlobe/></Button>
-                  </ButtonGroup>
-                  <Modal isOpen={this.show} >
-                    <ModalHeader>{this.state.modaltitle}</ModalHeader>
-                    <ModalBody>{this.state.modalinfo}</ModalBody>
-                    <ModalFooter>
-                      <Button color="secondary" onClick={(e)=>this.handleShow(e, "" , "" )}>Return</Button>
-                    </ModalFooter>
-                  </Modal>
-                  {/* <infomodal/> */}
-
-                </td>
-                           
+                <ButtonGroup size="sm">
+                  <ButtonDropGroup element={element.TELEFONO} icon="iconPhone"/>
+                  <ButtonDropGroup element={element.EMAIL} icon="iconMail"/>
+                  <ButtonDropGroup element={element.DIRECCION} icon="iconAdd"/>
+                  <Button href={element["CONTENT-URL"]}><FaGlobe/></Button>
+                </ButtonGroup>
+                </td>                           
               </tr>
                 )})}
           </tbody>
@@ -107,47 +71,39 @@ export default class ShowAll extends Component{
 }
 
 
-function AllParkings() {
-
-
-  // const infomodal = (datainfo) => {
-  //   return(
-  //     <Modal show={show} onHide={handleClose}>
-  //       <ModalBody>{datainfo}</ModalBody>
-  //     </Modal>
-  //   )
-  // }
-
-  // const toggle = () => setModal(!modal);
+class ButtonDropGroup extends Component{
+  constructor (props){
+    super(props)  
+    this.state = {
+      isOpen: false
+    }
+    // this.toggle = this.toggle.bind(this);
+  }
   
-  // return (
-    
-  //   )
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  
+  
+  render() {
+    console.log("WEP")
+    let but;
+    if(this.props.icon == "iconPhone" ){
+      but = <FaPhone/>
+    } else if ( this.props.icon == "iconMail" ){
+      but = <FaRegEnvelope/>
+    } else if( this.props.icon == "iconAdd" ){
+      but = <FaMapMarkerAlt/>
+    }
+    return (       
+      <Dropdown isOpen={this.state.isOpen} toggle={this.toggle}>
+        <DropdownToggle >{but}</DropdownToggle> 
+        <DropdownMenu><DropdownItem>{this.props.element}</DropdownItem></DropdownMenu>
+      </Dropdown >
+    )
+  }
+
+
 }
-
-
-
-
-
-{/* 
-            <Card className="card text-white bg-info mb-3 parkings-card" >
-              <div className="card-header card-title">{element.NOMBRE[1]}</div>
-              <div className="card-content">
-                <CardText>{element.DIRECCION}</CardText>
-                <CardText>{element.BARRIO}, {element.DISTRITO}</CardText>
-                 <Link to={`/parking/${element.PK}`}> <Button >See on map</Button> </Link> 
-                <a className="btn btn-primary" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample">
-    Link with href
-  </a>
-              </div>
-            </Card>
-            <div className="collapse" id="collapseExample">
-              <div className="card card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-              </div>
-            </div> 
-          </Col>*/}
-
-
-
-// ReactDOM.render(<ShowAll/>, document.getElementById('app'));
